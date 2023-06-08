c8da2299a6f20e1b1021c53f7f4c6262dc6c3a9a
SerhiiCheremisin <sebanho@gmail.com>
2023-06-08 19:52:20 +0200

    feat: changed logic of My Dictionary component

--- components/training/MyDictionary.tsx
+++ components/training/MyDictionary.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
 import useUserData from '../../services/hooks/useUserData';
 import { IRootDictionary } from '../../services/types';
 import { MyDictionaryCard, H3, PressableButton } from '../../services/styles';
@@ -13,11 +13,22 @@ const MyDictionary = () => {
   const userData = useUserData();
   const { name, dictionary } = userData;
   const [password, setPassword] = useState<string>("");
+  const [dictionaryCategories, setDictionaryCategories] = useState<string[]>([]);
+  const [activeCategoty, setActiveCategory] = useState<string>("");
   const dispatch = useDispatch();
 
   useEffect( () => {
     getLocalDataName().then( (data:ILocalStorageData) => setPassword(data.userPassword));
-  },[dictionary])
+  },[dictionary]);
+
+  useEffect( () => {
+   const uniqueDictionary :string [] = [];
+   dictionary.map( (el:IRootDictionary) => {
+      uniqueDictionary.push(el.category);
+      })
+   const set = new Set(uniqueDictionary);
+   setDictionaryCategories([...set]);
+  }, [])
 
   const deleteWordFromDictionary = (currentWord: IRootDictionary):void => {
     const newDictionary = [...dictionary].filter( (word:IRootDictionary) => word.sloWord !== currentWord.sloWord);
@@ -29,23 +40,74 @@ const MyDictionary = () => {
     dispatch(setUserDictionary(newDictionary));
   } 
 
+  const CardList = () => {
+    return (
+        <>
+        <Pressable onPress={() => setActiveCategory("")} style={styles.backButton}>
+          <Text>{`До списку категорій`}</Text>
+        </Pressable>
+         {dictionary.filter( (item: IRootDictionary) => item.category === activeCategoty)
+         .map( (item:IRootDictionary, idx: number) => {
+          return (
+        <MyDictionaryCard key={idx}>
+           <H3>{`${item.sloWord.toUpperCase()} - ${item.ukrWord.toUpperCase()}`}</H3>
+           {item.isIrregular === true && <H3>{`Теперішній час "${item.irregulars.present.toUpperCase()}", майбутній та минулий час "${item.irregulars.pastAndFuture.toUpperCase()}"`}</H3>}
+           <PressableButton onPress={() => deleteWordFromDictionary(item)}>
+            <Text>Видалити це слово</Text>
+           </PressableButton>
+        </MyDictionaryCard>
+      )
+ } )} 
+        </>
+     )
+  }
+
   return (
     <View style={{marginBottom: 50}}>
-     {dictionary.map( (item:IRootDictionary, idx: number) => {
-          return (
-            <MyDictionaryCard key={idx}>
-               <H3>{`${item.sloWord.toUpperCase()} - ${item.ukrWord.toUpperCase()}`}</H3>
-               {item.isIrregular === true && <H3>{`Теперішній час "${item.irregulars.present.toUpperCase()}", майбутній та минулий час "${item.irregulars.pastAndFuture.toUpperCase()}"`}</H3>}
-               <PressableButton onPress={() => deleteWordFromDictionary(item)}>
-                <Text>Видалити це слово</Text>
-               </PressableButton>
-            </MyDictionaryCard>
-          )
-     } )} 
+     { activeCategoty === "" ?
+        <ScrollView style={styles.viewWrapper}>
+        { dictionaryCategories.map( (el:string) =>  {
+         return (
+          <Pressable onPress={() => setActiveCategory(el)} style={styles.viewCard}>
+            <Text style={{fontSize: 20}}>{`${el}`}</Text>
+          </Pressable>
+         )
+        }
+ ) }
+        </ScrollView> 
+       :
+       <CardList/>  } 
     </View>
 
   )
 }
 
+const styles = StyleSheet.create({
+  viewWrapper: {
+    display: "flex",
+  },
+  viewCard : {
+    width: 250,
+    height: 100,
+    backgroundColor: "#dfff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius:10,
+  } ,
+  backButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 150,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginBottom: 15
+  }
+})
 
 export default MyDictionary;
